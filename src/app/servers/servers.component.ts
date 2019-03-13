5ec6a4ab31bb5cab430a7f730591a2e05045147a
ritoshyadav <ritosh90@gmail.com>
2019-03-14 00:28:26 +0530

    Add teamplate and provied styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',//you are just use template:`<html><titile></title......</html>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
